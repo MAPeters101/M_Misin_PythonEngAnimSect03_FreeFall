9baf7b9b557ee783afe49bba55f66896ab54c946
Mark Peters <mapeters101@earthlink.net>
2024-10-24 19:00:23 -0400

    Lesson059:  Complete lesson.

--- python_Earth_Mars_Moon.py
+++ python_Earth_Mars_Moon.py
@@ -16,10 +16,36 @@ g_Earth=-9.8 # [m/s^2]
 n=2
 y_i=100 # [m]
 y_Earth=y_i+0,5*g_Earth*t**n
+#np.set_printoptions(suppress=True)
 
 # Velocity y arrays
 y_Earth_velocity=n*0.5*g_Earth*t**(n-1)
 
 # Acceleration y arrays
 y_Earth_acceleration=(n-1)*g_Earth*t**(n-2)
+
+# Create circles
+def create_circle(r):
+    degrees=np.arange(0,361,1)
+    radians=degrees*np.pi/180
+    sphere_x=r*np.cos(radians)
+    sphere_y=r*np.sin(radians)
+    return sphere_x,sphere_y
+
+radius=5
+sphere_x_Earth,sphere_y_Earth=create_circle(radius)
+
+# np.set_printoptions(suppress=True)
+# print(sphere_x_Earth)
+# print(sphere_y_Earth)
+# exit()
+
+
+
+
+
+
+
+
+
 
