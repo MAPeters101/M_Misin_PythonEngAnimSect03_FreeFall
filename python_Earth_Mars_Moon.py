73bfb8d5fad6aa2d4fe8b54ea250103377b38c1b
Mark Peters <mapeters101@earthlink.net>
2024-10-24 20:37:10 -0400

    Lesson066:  Work on lesson from 3:40 to 7:50.

--- python_Earth_Mars_Moon.py
+++ python_Earth_Mars_Moon.py
@@ -80,12 +80,34 @@ plt.yticks(np.arange(y_f,y_i+2*dy,dy))
 plt.ylabel('altitude [m]')
 plt.title('Earth')
 
+# Create object for Mars
+ax1=fig.add_subplot(gs[:,1],facecolor=(0.9,0.9,0.9))
+sphere_Mars,=ax1.plot([],[],'k',linewidth=3)
+land_Mars=ax1.plot([-radius*width_ratio,radius*width_ratio],[-5,-5],'orangered',linewidth=42)
+plt.xlim(-radius*width_ratio,radius*width_ratio)
+plt.ylim(y_f,y_i+dy)
+plt.xticks(np.arange(-radius,radius+1,radius))
+plt.yticks(np.arange(y_f,y_i+2*dy,dy))
+plt.title('Mars')
+
+# Create object for Moon
+ax2=fig.add_subplot(gs[:,2],facecolor=(0.9,0.9,0.9))
+sphere_Moon,=ax2.plot([],[],'k',linewidth=3)
+land_Moon=ax2.plot([-radius*width_ratio,radius*width_ratio],[-5,-5],'gray',linewidth=42)
+plt.xlim(-radius*width_ratio,radius*width_ratio)
+plt.ylim(y_f,y_i+dy)
+plt.xticks(np.arange(-radius,radius+1,radius))
+plt.yticks(np.arange(y_f,y_i+2*dy,dy))
+plt.title('Moon')
+
 
 
 
 # Create position function
 ax3=fig.add_subplot(gs[0,3],facecolor=(0.9,0.9,0.9))
 alt_E,=ax3.plot([],[],'',linewidth=3,label='Alt_Earth = '+str(y_i)+' + ('+str(round(g_Earth/2,1))+')t^'+str(n)+' [m]')
+alt_Ma,=ax3.plot([],[],'orangered',linewidth=3,label='Alt_Mars = '+str(y_i)+' + ('+str(round(g_Mars/2,1))+')t^'+str(n)+' [m]')
+alt_Mo,=ax3.plot([],[],'gray',linewidth=3,label='Alt_Moon = '+str(y_i)+' + ('+str(round(g_Moon/2,1))+')t^'+str(n)+' [m]')
 plt.xlim(0,t_end)
 plt.ylim(0,y_i)
 plt.legend(loc=(0.6,0.7),fontsize='x-small')
@@ -93,6 +115,8 @@ plt.legend(loc=(0.6,0.7),fontsize='x-small')
 # Create velocity function
 ax4=fig.add_subplot(gs[1,3],facecolor=(0.9,0.9,0.9))
 vel_E,=ax4.plot([],[],'',linewidth=3,label='Vel_Earth = '+str(g_Earth)+'t [m/s]')
+vel_Ma,=ax4.plot([],[],'orangered',linewidth=3,label='Vel_Mars = '+str(g_Earth)+'t [m/s]')
+vel_Mo,=ax4.plot([],[],'gray',linewidth=3,label='Vel_Moon = '+str(g_Earth)+'t [m/s]')
 plt.xlim(0,t_end)
 plt.ylim(y_Earth_velocity[-1],0)
 plt.legend(loc='lower left',fontsize='x-small')
@@ -100,6 +124,8 @@ plt.legend(loc='lower left',fontsize='x-small')
 # Create acceleration function
 ax5=fig.add_subplot(gs[2,3],facecolor=(0.9,0.9,0.9))
 acc_E,=ax5.plot([],[],'',linewidth=3,label='Acc_Earth = '+str(g_Earth)+' [(m/s)/s == m/s^2]')
+acc_Ma,=ax5.plot([],[],'orangered',linewidth=3,label='Acc_Mars = '+str(g_Earth)+' [(m/s)/s == m/s^2]')
+acc_Mo,=ax5.plot([],[],'gray',linewidth=3,label='Acc_Moon = '+str(g_Earth)+' [(m/s)/s == m/s^2]')
 plt.xlim(0,t_end)
 plt.ylim(g_Earth-1,0)
 plt.legend(loc=(0.02,0.25),fontsize='x-small')
